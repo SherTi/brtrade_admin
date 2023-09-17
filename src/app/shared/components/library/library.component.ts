9d9c91fa6ecfa713d8059fb68c1f3eacc0c9ec23
SherTi <tulkunovsher69@gmail.com>
2023-09-18 03:05:58 +0500

    some editions

--- src/app/shared/components/library/library.component.ts
+++ src/app/shared/components/library/library.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 
+
 @Component({
   selector: '.app-library',
   templateUrl: './library.component.html',
@@ -8,12 +9,57 @@ import {Component, Input} from '@angular/core';
 export class LibraryComponent {
   items:any [] = [
     {
-
-    }
+      id: 1,
+      name: "Тракторк",
+      width: "150px",
+      height: "123px",
+      size: "150x123",
+      selected: false,
+      createdAt: "",
+      createdUp: "",
+    },
+    {
+      id: 2,
+      name: "Тракторк",
+      width: "150px",
+      height: "123px",
+      size: "150x123",
+      selected: false,
+      createdAt: "",
+      createdUp: "",
+    },
+    {
+      id: 3,
+      name: "Тракторк",
+      width: "150px",
+      height: "123px",
+      size: "150x123",
+      selected: false,
+      createdAt: "",
+      createdUp: "",
+    },
+    {
+      id: 4,
+      name: "Тракторк",
+      width: "150px",
+      height: "123px",
+      size: "150x123",
+      selected: false,
+      createdAt: "",
+      createdUp: "",
+    },
   ]
-  check: boolean = false
-  selected: boolean = false
-  onClick() {
 
+  check: boolean = false
+  newArr : any = []
+  onChange() {
+    this.newArr.filter(() => {
+
+      console.log(this.newArr)
+    })
+  }
+  onClick(item: any) {
+    item.selected = true;
+    console.log(item)
   }
 }

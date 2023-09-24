8633038cbf2fe77b9b69a9b04734de7863aaa1f9
SherTi <tulkunovsher69@gmail.com>
2023-09-24 22:34:14 +0500

    some editions

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {LibraryComponent} from "../../shared/components/library/library.component";
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
-export class SettingsComponent {
+export class SettingsComponent  extends LibraryComponent{
 
   switch:boolean = true
+  modalWindow: boolean = false
+  modalWin() {
+    this.modalWindow = !this.modalWindow
+    console.log("click")
+    this.submit()
+  }
 }
